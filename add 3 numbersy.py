f019700fc0d3392a293eb1043871c504bdd6718c
NKDLW <nk01dlw@gmail.com>
2023-05-04 11:35:58 +0200

    Add new training feature
    Here we describe how to add THREE numbers!

---
+++ add 3 numbersy.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+# Works with Python 3.6 version and above.
+
+# Set the values.
+num1 = 1.5
+num2 = 6.8
+num3 = 16.8
+
+# Display the sum.
+print(f'The sum of {num1} and {num2}and {num3} is {num1 + num2 + num3}.')
